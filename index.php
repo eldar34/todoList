f49731e82518a6219fec1aebdc9af10fdfda1bc0
eldar <eldar@example.com>
2020-05-17 08:31:51 +0300

    Upload enviroments

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-if(!getenv('LOCAL_ENV')){
+if(getenv('LOCAL_ENV') == false){
     $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ );
     $dotenv->load();
 }
